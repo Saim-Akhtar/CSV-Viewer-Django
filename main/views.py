a3966316a4db1bb2749d9526fefcc24d18459b67
Saim-Akhtar <saimakhtar98@gmail.com>
2023-11-20 17:24:08 +0200

    added detail record condition

--- main/views.py
+++ main/views.py
@@ -4,8 +4,11 @@ from .dataloader import load_data
 
 def patient_detail(request, patient_id):
     data_df = load_data()
-    patient = data_df[data_df['patient_id'] == patient_id].to_dict('records')[0]
-    patient['image_path'] = f'/media/images/{patient["patient_id"]}-{patient["series_uid"]}.jpg'
+    try:
+        patient = data_df[data_df['patient_id'] == patient_id].to_dict('records')[0]
+        patient['image_path'] = f'/media/images/{patient["patient_id"]}-{patient["series_uid"]}.jpg'
+    except: 
+        patient = None
     return render(request, 'detail.html', {'patient': patient})
 
 def datatable_view(request):
